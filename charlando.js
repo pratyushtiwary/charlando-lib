7322b3200bffba27a22d0e62d76c1e9d0ed276ee
Pratyush Tiwary <cantaloupeinc.1@gmail.com>
2023-01-14 22:55:13 +0530

    App name is now displayed and error messages are much more nice looking

--- charlando.js
+++ charlando.js
@@ -123,7 +123,11 @@
   }
 
   function throwError(msg) {
-    throw new Error("CharLando: " + msg);
+    if (typeof msg === "string") {
+      throw new Error("CharLando: " + msg);
+    } else if (typeof msg === "object") {
+      throw new Error("CharLando: " + JSON.stringify(msg));
+    }
   }
 
   class Utils {
@@ -222,7 +226,7 @@
       statusIndicator.appendChild(status);
       statusIndicator.appendChild(statusText);
 
-      title.innerText = "CharLando";
+      title.innerText = this.name || "CharLando";
       close.innerHTML =
         '<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16"><path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/></svg>';
 
@@ -390,6 +394,8 @@
           initialData = data.success.data;
 
           self.fallback = data.success.data.fallback.value;
+
+          self.name = data.success.data.name;
 
           function resetUserActivityTimeout() {
             self.#resetUserActivityTimeout();
